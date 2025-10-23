ebd8431f80b12a8af6b7d43f6d2b9ea946e3f851
SandwichPresident <shb1713@rit.edu>
2025-10-23 08:15:15 -0400

    Created doc and found Mean

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            Console.Write("n = ");
+            int.TryParse(Console.ReadLine(), out int n);
+            if (n == 0) { Console.WriteLine("No result."); }
+            int i = 0;
+            do
+            {
+                Console.Write("a = ");
+                double.TryParse(Console.ReadLine(), out double a);
+                sum += a;
+                i++;
+            }
+            while (i != n);
+            double result = sum / n;
+            Console.WriteLine($"Result: {result:F2}");
+        }
+    }
+}
